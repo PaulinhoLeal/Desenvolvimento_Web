2c8796ba8176cd0467237246f6755ffc8694fb1b
PaulinhoLeal <paulonagasumileal@gmail.com>
2023-04-10 13:24:33 -0300

    Exercicio da banda

---
+++ Listas/Lista-3/Exercicio1.js
@@ -0,0 +1,8 @@
+var banda = {
+    nome: 'Viajantes',
+    anoPrieiroCd: '2001',
+    melhoresMusicas: ['navegando','sol do inverno','inverno tropical'],
+    integrantes: [[{nome: 'João costa'},{paisOrigem: 'Brasíl'},{idade: 20}],
+                [{nome: 'Lúcio pizza'},{paisOrigem: "Itália"},{idade: 25}],
+                [{nome: 'Humberto haung'},{paisOrigem:'China'},{idade: 30}]]
+}
