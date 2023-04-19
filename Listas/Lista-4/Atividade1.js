e1b360c3a5b830633ef4412e3ef41634faf857a2
PaulinhoLeal <paulonagasumileal@gmail.com>
2023-04-19 16:07:47 -0300

    Lista 4

---
+++ Listas/Lista-4/Atividade1.js
@@ -0,0 +1,26 @@
+var ContaCorrente = 
+{
+    saldo:'',
+    faturaAtual:0,
+    creditoDisponivel:0,
+    extrato: 
+    {
+        entrada:
+        [
+            {
+                data:'',
+                valor:0
+            }
+        ]
+        saida:
+        [
+            {
+                data:'',
+                valo:0,
+                descricao:''
+            }
+        ]
+    },
+    exibirExtrato:
+
+}
